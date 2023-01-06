ed008cc4bca50a61e9cf1faad0a29d3e7073308c
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-01-07 00:53:51 +0200

    feat(bot): login

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,3 +1,13 @@
-import { serverFunc } from "server"
+import { Client } from "discord.js"
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-console.log(serverFunc())
+const bot = new Client({
+    intents: ["Guilds", "DirectMessages", "MessageContent"]
+})
+
+bot.on("ready", () => {
+    console.log(`bot ready in ${bot.guilds.cache.size} guilds`)
+})
+
+bot.login(process.env.BOT_TOKEN)
