ef5c34197e9f37e5a4b5f84b0f458916897f17c1
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-01-07 02:51:38 +0200

    chore: remove unused log

--- packages/server/src/db.ts
+++ packages/server/src/db.ts
@@ -3,5 +3,3 @@ import { PrismaClient } from "@prisma/client";
 export const serverFunc = () => "Hello from server"
 
 const prisma = new PrismaClient();
-
-prisma.guild.findMany().then(r => console.table(r))
