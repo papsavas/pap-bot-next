1e00926a41eb9b4df267cb634a1535061df998fa
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-01-07 17:28:37 +0200

    chore: move client events to client

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,11 +1,17 @@
 import { Client } from "discord.js";
 import * as dotenv from 'dotenv';
-import { eventFiles } from "server";
+import { readdirSync } from "node:fs";
+import { DiscordEventFile } from "./types/DiscordEvent";
 dotenv.config()
 
 const bot = new Client({
     intents: ["Guilds", "DirectMessages", "MessageContent"]
 })
+
+const eventFiles: DiscordEventFile[] = readdirSync("/ClientEvents/")
+    .map(file => require(`@bot/src/Events/Impl/${file}`).default);
+
+
 
 eventFiles.forEach(ev => bot.on(ev.name,
     async (...args) => { ev.execute(...args) })
--- packages/server/src/events.ts
+++
@@ -1,5 +0,0 @@
-import { DiscordEventFile } from "@bot/src/types/DiscordEvent";
-import { readdirSync } from "node:fs";
-export const eventFiles: DiscordEventFile[] = readdirSync("@bot/src/Events/")
-    .map(file => require(`@bot/src/Events/Impl/${file}`).default);
-
