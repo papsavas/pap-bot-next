85f466dd33eba7ab74a64deb11d629631ac6b3b8
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-01-19 00:08:42 +0200

    fix: rename export

--- apps/bot/src/Events/messageCreate.ts
+++ apps/bot/src/Events/messageCreate.ts
@@ -1,6 +1,6 @@
 import { makeEvent } from "./makeEvent"
 
-export const message = makeEvent({
+export const messageCreate = makeEvent({
     name: "messageCreate",
     async execute(message) {
         return Promise.reject(`${name} has no execution implemented`)
