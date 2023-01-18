062b4e2de3a8985e9fd5b03d7da2870de45aad0d
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-01-19 00:33:44 +0200

    handle `ready`

--- apps/bot/src/Events/ready.ts
+++ apps/bot/src/Events/ready.ts
@@ -3,6 +3,6 @@ import { makeEvent } from "./makeEvent"
 export const ready = makeEvent({
     name: "ready",
     async execute(client) {
-        return Promise.reject(`${name} has no execution implemented`)
+        console.log(`Bot cache ready. Serving ${client.guilds.cache.size} servers`)
     },
 })
