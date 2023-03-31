473a609847493cd42bdf568685c106f1ecb96934
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-03-31 17:49:42 +0300

    feat(bot_scripts): migrate channel messages

--- apps/bot/src/scripts/migrateChannel.ts
+++ apps/bot/src/scripts/migrateChannel.ts
@@ -1,73 +1,18 @@
-import { AttachmentBuilder, AttachmentPayload, Collection, JSONEncodable, Message, Partials, Snowflake, TextChannel, WebhookClient } from "discord.js";
+import { Partials } from "discord.js";
+import { migrateMessages } from "../utils/migrateMessages";
 import client from "./client";
-
-const MAX_LIMIT_MESSAGE_CHUNK = 100;
-
-const bot = await client(
-    ["GuildMembers", "GuildMessages", "GuildWebhooks", "Guilds"],
-    [Partials.Channel, Partials.GuildMember, Partials.Message, Partials.ThreadMember]
-)
 
 const [path, file, guildId, oldChannelId, newChannelId, ...rest] = process.argv;
 
 if (!(guildId && oldChannelId && newChannelId))
-    throw new Error("<guild_id> <old_channel_id> <new_channel_id> [limit]");
+    throw new Error("<guild_id> <old_channel_id> <new_channel_id>");
 
-const guild = await bot.guilds.fetch(guildId);
-console.log(`fetched ${guild.name}`);
-const oldChannel = await guild.channels.fetch(oldChannelId);
-console.log(`fetched old channel ${oldChannel?.name}`);
-const newChannel = await guild.channels.fetch(newChannelId);
-console.log(`fetched new channel ${newChannel?.name}`);
-if (!(oldChannel && newChannel)) throw new Error("Channels not found");
-if (!(oldChannel.isTextBased() && newChannel.isTextBased())) throw new Error("Channels are not text based");
+const bot = await client(
+    ["Guilds", "GuildMessages"],
+    [Partials.Channel, Partials.GuildMember, Partials.Message, Partials.ThreadMember]
+);
 
-const webhook = await (newChannel as TextChannel).createWebhook({
-    name: `mocking-users`,
-    reason: `Channel migration ${oldChannel.name} -> ${newChannel.name}`
-});
-
-const fetchMessages = async (beforeMsgId?: string) => {
-    const res = await oldChannel.messages.fetch({ limit: MAX_LIMIT_MESSAGE_CHUNK, before: beforeMsgId })
-    return res.reverse()
-}
-
-const collectAllMessages = async () => {
-    let allMessages = new Collection<Snowflake, Message<true>>();
-    let chunk = await fetchMessages();
-    allMessages = chunk.concat(allMessages);
-    while (chunk.size > 0) {
-        console.log(`fetching previous chunk`);
-        chunk = await fetchMessages(chunk.firstKey());
-        allMessages = chunk.concat(allMessages);
-    }
-    return allMessages;
-}
-let counter = 0;
-const allMessages = await collectAllMessages()
-for (const [msdId, oldMessage] of allMessages) {
-    counter++;
-    try {
-        const webhookClient = new WebhookClient({ url: webhook.url });
-        const newAPIMessage = await webhookClient.send({
-            username: `${oldMessage.author.tag}`,
-            avatarURL: oldMessage.author.avatarURL() ?? undefined,
-            content: oldMessage.content,
-            embeds: oldMessage.embeds,
-            components: oldMessage.components,
-            files: oldMessage.attachments.map(a => AttachmentBuilder
-                .from(a as JSONEncodable<AttachmentPayload>).setName(a.name)
-            )
-        })
-        if (oldMessage.pinned) {
-            const newMessage = await newChannel.messages.fetch(newAPIMessage.id);
-            console.log(`should pin ${newMessage.url}`);
-            newMessage.pinnable && newMessage.pin("pinned in old channel")
-        }
-        console.log(`(${counter}/${allMessages.size}) moved ${oldMessage.url}`);
-    } catch (error) {
-        console.error(error)
-    }
-}
-await webhook.delete("migration completed");
-process.exit(0);
+bot.on("ready", async client => {
+    await migrateMessages(client, guildId, oldChannelId, newChannelId);
+    process.exit(0);
+})
