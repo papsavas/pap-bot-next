7037f7523ea1fe84c835ee00c25fbcfd5c3e2f2b
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-04-01 12:56:57 +0300

    fix(ypes): cover more structures

--- apps/site/app/components/GuildLink.tsx
+++ apps/site/app/components/GuildLink.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 import { FC } from "react";
 import { Guild, JSON } from "types";
 const GuildLink: FC<{ guild: JSON<Guild> }> = ({ guild }) => {
-  const { id, name } = guild;
+  const { id, name, iconURL } = guild;
   return (
     <li id={id} className="flex list-none flex-col items-center">
       <Link href={`/guilds/${id}`} className="text-xl">
         <img
-          src={guild.iconURL!}
+          src={iconURL!}
           alt={`${name} picture`}
           className="rounded-2xl object-contain"
         />
--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,12 +1,13 @@
+import { DataManager } from "discord.js";
 export { Collection, Guild, Snowflake } from "discord.js";
 
-type FunctionResult<T> = {
-    [K in keyof T]: T[K] extends (...args: any) => any
-    ? ReturnType<T[K]>
-    : never
+export type JSON<T> = {
+    [K in keyof T]:
+    T[K] extends (args: any) => infer R ? R :
+    T[K] extends Array<infer U> ? Array<JSON<U>> :
+    T[K] extends Map<infer K, infer V> ? JSON<K> :
+    T[K] extends DataManager<infer K, infer Holds, infer R> ? JSON<K[]> :
+    T[K] extends string | number ? T[K] :
+    JSON<T[K]>;
 }
 
-export type JSON<T> =
-    T extends Array<infer U>
-    ? Array<FunctionResult<U>>
-    : FunctionResult<T>
