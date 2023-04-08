6c0afea0de751520a493d77cd12eadda266bdcb6
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-04-08 18:57:37 +0300

    fix(site:guildnav): fallback

--- apps/site/app/components/GuildNavBar/GuildNavBar.tsx
+++ apps/site/app/components/GuildNavBar/GuildNavBar.tsx
@@ -13,10 +13,12 @@ const GuildNavBar = () => {
       className={`flex w-[${blockSize}px] aspect-square flex-shrink list-none flex-col gap-5 bg-neutral-800 px-2 py-6 dark:bg-neutral-800`}
     >
       {isLoading
-        ? Array(Math.trunc(window.innerHeight / blockSize))
+        ? Array(Math.round(window.innerHeight / blockSize))
             .fill(0)
-            .map(() => <span className="animate-pulse"></span>)
-        : guilds!.map((g) => <GuildLink guild={g} key={g.id} />)}
+            .map(() => (
+              <span className="flex-1 animate-pulse rounded-xl bg-neutral-900"></span>
+            ))
+        : guilds?.map((g) => <GuildLink guild={g} key={g.id} />)}
     </nav>
   );
 };
