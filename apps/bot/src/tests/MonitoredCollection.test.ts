bca4e0212bbc25229e9170617205837ba5360cfe
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-04-19 01:27:04 +0300

    tests(bot): monitored collection

---
+++ apps/bot/src/tests/MonitoredCollection.test.ts
@@ -0,0 +1,28 @@
+import { describe, it, vi } from "vitest";
+import { MonitoredCollection } from "../utils/MonitoredCollection";
+
+describe('Monitored Collection', () => {
+    const [k, v] = ["key", "val"];
+    const mockMonitor = vi.fn();
+    const mc = new MonitoredCollection<string, string>(undefined, {
+        get: mockMonitor,
+        set: mockMonitor,
+        delete: mockMonitor
+    });
+
+    it('should trigger monitor', () => {
+        mockMonitor.mockClear();
+        mc.set(k, v, true);
+        mc.get(k, true);
+        mc.delete(k, true);
+        expect(mockMonitor).toBeCalledTimes(3)
+    });
+
+    it('should not trigger monitor', () => {
+        mockMonitor.mockClear();
+        mc.set(k, v);
+        mc.get(k, false);
+        mc.delete(k);
+        expect(mockMonitor).not.toBeCalled()
+    });
+});
