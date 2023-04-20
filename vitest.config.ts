ff3fc7fbff983b5a94da496a81d142ed56ce33d7
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-04-20 22:14:58 +0300

    passwithNoTest and tsc typecheck

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        // include: [
-        //     "{apps,packages}/**/*.{test,spec}.{js, jsx, ts, tsx}",
-        //     "{apps,packages}/**/*.{test,spec}-d.ts",
-        // ],
         exclude: [
             "**/node_modules", "**dist", "**/next"
         ],
+        passWithNoTests: true,
+        typecheck: { checker: "tsc" },
         coverage: {
             enabled: true,
             all: true,
