11a799d5c77746aa051c9da56686c719e4f20991
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-04-27 21:41:04 +0300

    feat: prisma agnostic `makeScript` helper

--- packages/database/tests/guild.test.ts
+++ packages/database/tests/guild.test.ts
@@ -5,11 +5,11 @@ import prisma from './__mocks__/prisma';
 vi.mock('../prisma')
 
 describe("guild", () => {
-        const mockGuild = { name: "g_name", icon: "g_icon", id: "g_id" };
-        it('should create guild', async () => {
-                prisma.guild.create.mockResolvedValue(mockGuild);
-                const guild = await createGuild(mockGuild, prisma);
-                expect(guild).toStrictEqual(mockGuild)
-        });
+    const mockGuild = { name: "g_name", icon: "g_icon", id: "g_id" };
+    it('should create guild', async () => {
+        prisma.guild.create.mockResolvedValue(mockGuild);
+        const guild = await createGuild(mockGuild, prisma);
+        expect(guild).toStrictEqual(mockGuild)
+    });
 })
 
