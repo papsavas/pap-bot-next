b4b2040bf3e7d9890827d2c18126f23b97f8d92a
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-06 17:23:58 +0300

    feat(site): show username in `UserButton`

--- apps/site/src/components/Navbar.tsx
+++ apps/site/src/components/Navbar.tsx
@@ -6,7 +6,16 @@ const NavBar = () => {
     <nav className="flex h-12 w-full items-center bg-neutral-900">
       <div className="mx-16 flex w-full flex-1 flex-row items-center justify-between text-white">
         <NavButton label="Home" path="/" />
-        <UserButton afterSignOutUrl="/login" />
+        <UserButton
+          afterSignOutUrl="/login"
+          showName={true}
+          userProfileMode="modal"
+          appearance={{
+            elements: {
+              userButtonOuterIdentifier: "text-lightText dark:text-darkText",
+            },
+          }}
+        />
       </div>
     </nav>
   );
