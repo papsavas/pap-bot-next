ce53369db99de96e4cb7ffc2d8311462f70911e0
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-07 12:42:25 +0300

    chore(site): use contract endpoint

--- apps/site/src/lib/ts-rest.ts
+++ apps/site/src/lib/ts-rest.ts
@@ -1,9 +1,8 @@
 import { initClient } from "@ts-rest/core";
-import { contract } from "http-contract";
+import { BOT_ENDPOINT, contract } from "http-contract";
 
 export const tsRest = initClient(contract, {
-    //TODO: imported `BOT_ENDPOINT` undefined on load
-    baseUrl: "http://localhost:4040",
+    baseUrl: BOT_ENDPOINT,
     baseHeaders: {
 
     }
