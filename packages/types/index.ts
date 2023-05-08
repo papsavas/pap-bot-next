2e0bda4d73935f97023ef655639f33bf740ec7a8
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-08 12:51:56 +0300

    types: export `JSON` helper

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,4 +1,3 @@
 export { guildMemberIdQuery, guildObject, type Guild } from "./Guild";
+export { JSON } from "./JSON";
 export { prefixObject, prefixPathParams, prefixWithoutGuildIdObject, type Prefix } from "./Prefix";
-
-
