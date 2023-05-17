e847892397ca884213454cc1f789e94a4649bf0e
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-17 15:26:35 +0300

    feat(bot): message commands

--- apps/bot/src/events/messageCreate.ts
+++ apps/bot/src/events/messageCreate.ts
@@ -1,8 +1,20 @@
+import { ctx } from "..";
+import { sliceCommand } from "../lib/commands/slice";
+import { NotServedError } from "../lib/errors";
 import { makeEvent } from "../lib/makeEvent";
 
 const messageCreate = makeEvent({
     event: "messageCreate",
     async execute(message) {
+        if (message.inGuild()) {
+            const guildCommandPrefix = ctx.prefix.get(message.guildId)
+            if (!guildCommandPrefix) throw new NotServedError("messageCreate", message.guildId);
+            if (message.content.startsWith(guildCommandPrefix.prefix)) {
+                const { primaryCommand } = sliceCommand(message, guildCommandPrefix.prefix);
+                const cmd = ctx.commands.find(cmd => cmd.name === primaryCommand.toLowerCase());
+                cmd?.execute(message);
+            }
+        }
     }
 })
 
