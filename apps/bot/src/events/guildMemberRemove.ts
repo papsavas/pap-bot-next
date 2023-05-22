9337f4bb1e0c8720c84f332539866910ef537a3e
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-22 15:01:42 +0300

    fix(bot): Sweep reaction-notifiers in member remove

--- apps/bot/src/events/guildMemberRemove.ts
+++ apps/bot/src/events/guildMemberRemove.ts
@@ -1,9 +1,11 @@
+import { ctx } from "..";
 import { makeEvent } from "../lib/makeEvent";
 
 const guildMemberRemove = makeEvent({
     event: "guildMemberRemove",
     async execute(member) {
         console.log(`member ${member.user.tag} left ${member.guild.name}`);
+        ctx.reactionNotifier.sweep(rn => rn.userId === member.id, true);
     },
 })
 
